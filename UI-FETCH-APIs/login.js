a123967544c4b3fe5c5106238370d1eda74813a5
obulaworld <chisom@winexviv.com>
2018-08-01 16:15:05 +0000

    feat(fetch-api) Consume login api This feature builds out the fetch function to consume the /auth/login api

--- UI-FETCH-APIs/login.js
+++ UI-FETCH-APIs/login.js
@@ -8,6 +8,27 @@ const emailElement = document.getElementById('email');
 const passwordElement = document.getElementById('password');
 const loginButton = document.getElementById('login');
 const successElement = document.getElementById('success');
+let errorCount = 0;
+
+const checkEmail = (email) => {
+  // Reference => https://stackoverflow.com/questions/20301237/javascript-form-validating-e-mail-address-and-checking-another-field-with-star
+  const emailFilter = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/;
+  if (email === '' || !email.replace(/\s/g, '').length || !emailFilter.test(email)) {
+        errorCount += 1;
+        emailElement.style.border = '1px solid red';
+    } else {
+       emailElement.style.border = '1px solid green';
+   }
+};
+
+const checkPassword = (password) => {
+    if (password === '' || !password.replace(/\s/g, '').length) {
+        errorCount += 1;
+        passwordElement.style.border = '1px solid red';
+    } else {
+        passwordElement.style.border = '1px solid green';
+    }
+};
 
 const loginUser = (details) => {
 const url = 'https://my-diary-challenge.herokuapp.com/auth/login';
@@ -37,27 +58,22 @@ const url = 'https://my-diary-challenge.herokuapp.com/auth/login';
 };
 
 const checkLogin = () => {
-  let errorCount = 0;
-  const email = emailElement.value;
-  const password = passwordElement.value;
-  // Reference => https://stackoverflow.com/questions/20301237/javascript-form-validating-e-mail-address-and-checking-another-field-with-star
-  const emailFilter = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/;
-  if (email === '' || !email.replace(/\s/g, '').length || !emailFilter.test(email)) {
-    errorCount += 1; emailElement.style.border = '1px solid red';
-  } else { emailElement.style.border = '1px solid green'; }
-  if (password === '' || !password.replace(/\s/g, '').length) {
-    errorCount += 1; passwordElement.style.border = '1px solid red';
-  } else {
-    passwordElement.style.border = '1px solid green';
-  }
+  errorCount = 0;
+  const email1 = emailElement.value;
+  const password1 = passwordElement.value;
+  checkEmail(email1);
+  checkPassword(password1);
+
   if (errorCount > 0) {
     successElement.style.color = 'red';
     successElement.style.fontSize = '15px';
     successElement.innerHTML = 'The Inputs with red border are either empty or invalid';
-    return;
-  } else { successElement.innerHTML = ''; }
-  loginButton.innerHTML = 'Please wait';
-  loginButton.setAttribute('disabled', '');
-  const details = { password: password, email: email };
-  loginUser(details);
+    return false;
+  } else {
+    successElement.innerHTML = '';
+    loginButton.innerHTML = 'Please wait';
+    loginButton.setAttribute('disabled', '');
+    const details = { password: password1, email: email1 };
+    loginUser(details);
+  }
 };
