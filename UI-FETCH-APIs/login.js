bc68aea7e75f004c98e912686354d9bd2db19820
obulaworld <chisom@winexviv.com>
2018-08-01 15:34:49 +0000

    feat(fetch-api) Consume login api This feature builds out the fetch function to consume the /auth/login api

--- UI-FETCH-APIs/login.js
+++ UI-FETCH-APIs/login.js
@@ -42,33 +42,22 @@ const checkLogin = () => {
   const password = passwordElement.value;
   // Reference => https://stackoverflow.com/questions/20301237/javascript-form-validating-e-mail-address-and-checking-another-field-with-star
   const emailFilter = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/;
-
   if (email === '' || !email.replace(/\s/g, '').length || !emailFilter.test(email)) {
-    errorCount += 1;
-    emailElement.style.border = '1px solid red';
-  } else {
-    emailElement.style.border = '1px solid green';
-  }
-
+    errorCount += 1; emailElement.style.border = '1px solid red';
+  } else { emailElement.style.border = '1px solid green'; }
   if (password === '' || !password.replace(/\s/g, '').length) {
-    errorCount += 1;
-    passwordElement.style.border = '1px solid red';
+    errorCount += 1; passwordElement.style.border = '1px solid red';
   } else {
     passwordElement.style.border = '1px solid green';
   }
-
   if (errorCount > 0) {
     successElement.style.color = 'red';
     successElement.style.fontSize = '15px';
     successElement.innerHTML = 'The Inputs with red border are either empty or invalid';
     return;
-  } else {
-    successElement.innerHTML = '';
-  }
-
+  } else { successElement.innerHTML = ''; }
   loginButton.innerHTML = 'Please wait';
   loginButton.setAttribute('disabled', '');
   const details = { password: password, email: email };
-
   loginUser(details);
 };
