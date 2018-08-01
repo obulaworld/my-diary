d910df5973a1389070df59d44db71e4fa536fdb5
obulaworld <chisom@winexviv.com>
2018-08-01 14:17:04 +0000

    feat(fetch-api) Consume login api This feature builds out the fetch function to consume the /auth/login api

---
+++ UI-FETCH-APIs/login.js
@@ -0,0 +1,74 @@
+/**
+ * Created by obulaworld on 7/31/18.
+ */
+/**
+ * Created by obulaworld on 7/31/18.
+ */
+const emailElement = document.getElementById('email');
+const passwordElement = document.getElementById('password');
+const loginButton = document.getElementById('login');
+const successElement = document.getElementById('success');
+
+const loginUser = (details) => {
+const url = 'https://my-diary-challenge.herokuapp.com/auth/login';
+  fetch(url, { method: 'POST', headers: { 'Content-Type': 'application/json; charset=utf-8' }, body: JSON.stringify(details) })
+    .then((response) => {
+      return response.json();
+    }).then((data) => {
+      if (data.success) {
+        loginButton.innerHTML = 'Login';
+        loginButton.removeAttribute('disabled');
+        successElement.style.color = 'green';
+        successElement.innerHTML = 'Successful....Redirecting to Dashboard';
+        localStorage.setItem('token', data.token);
+        setTimeout(() => {
+          window.location.href = window.location.protocol + '//' + window.location.hostname + '/dashboard.html';
+        },2000);
+      } else if (data.error) {
+        loginButton.innerHTML = 'Login';
+        loginButton.removeAttribute('disabled');
+        successElement.style.color = 'red';
+        successElement.style.fontSize = '15px';
+        successElement.innerHTML = data.error;
+      }
+    }).catch((err) => {
+      console.log('Request failed', err);
+    });
+};
+
+const checkLogin = () => {
+  let errorCount = 0;
+  const email = emailElement.value;
+  const password = passwordElement.value;
+  // Reference => https://stackoverflow.com/questions/20301237/javascript-form-validating-e-mail-address-and-checking-another-field-with-star
+  const emailFilter = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/;
+
+  if (email === '' || !email.replace(/\s/g, '').length || !emailFilter.test(email)) {
+    errorCount += 1;
+    emailElement.style.border = '1px solid red';
+  } else {
+    emailElement.style.border = '1px solid green';
+  }
+
+  if (password === '' || !password.replace(/\s/g, '').length) {
+    errorCount += 1;
+    passwordElement.style.border = '1px solid red';
+  } else {
+    passwordElement.style.border = '1px solid green';
+  }
+
+  if (errorCount > 0) {
+    successElement.style.color = 'red';
+    successElement.style.fontSize = '15px';
+    successElement.innerHTML = 'The Inputs with red border are either empty or invalid';
+    return;
+  } else {
+    successElement.innerHTML = '';
+  }
+
+  loginButton.innerHTML = 'Please wait';
+  loginButton.setAttribute('disabled', '');
+  const details = { password: password, email: email };
+
+  loginUser(details);
+};
