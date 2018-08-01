1c36f73cc8d97fb8ed7ec0f43689187a3b80d2ed
obulaworld <chisom@winexviv.com>
2018-08-01 19:14:08 +0000

    feat(fetch-api) Consume login api This feature builds out the fetch function to consume the /auth/login api

--- server/middlewares/Validation.js
+++ server/middlewares/Validation.js
@@ -1,20 +1,26 @@
-import Joi from 'joi';
-
+let pass = true;
 const check = {
   validate(req, res, next) {
-    const schema = Joi.object().keys({
-      title: Joi.string().required(),
-      category: Joi.string().required(),
-      subCategory: Joi.string().required(),
-      content: Joi.string().required(),
-    });
-    const { error } = Joi.validate(req.body, schema);
-    if (error) {
-      res.status(400).json({error: error.details[0].message});
-    } else {
-      next();
+      const values = req.body;
+      const required = ['title', 'category', 'subCategory', 'content'];
+      let errors = {};
+      for (let i = 0; i < required.length; i += 1) {
+          if (!values[required[i]]) { pass = false; errors[required[i]] = `${required[i]} not sent`; }
+      }
+    if(values.title && !values.title.replace(/\s/g, '').length) {
+        errors.title = 'Title can not be empty or spaces'; pass = false;
     }
-  },
+   if(values.category && !values.category.replace(/\s/g, '').length) {
+      errors.category = 'Category can not be empty or spaces'; pass = false;
+   }
+  if(values.subCategory && !values.subCategory.replace(/\s/g, '').length) {
+      errors.subCategory = 'Subcategory can not be empty or spaces'; pass = false;
+  }
+  if(values.content && !values.content.replace(/\s/g, '').length) {
+      errors.content = 'Content can not be empty or spaces';
+      pass = false;
+  }
+    if (pass === false) { res.status(400).json({ error: errors }); } else { next(); } },
 };
 
 export default check;
