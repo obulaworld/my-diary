4bec8c55fed0442c35e2acb7fcbe95464f752177
obulaworld <chisom@winexviv.com>
2018-08-19 21:39:44 +0000

    chore(refactor-codes) refactor lines of codes Refactor all lines of codes in code climate's review [Delivers #159875530]

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -14,16 +14,12 @@ class UserController {
           return client.query(check, (error2, res2) => {
               if (error2) { res.status(400).json({ success: false, message: 'Something went wrong with the process, Please try later' });
               }else{
-                  if (res2.rows.length) {
-                       res.status(409).json({ success: false, message: `Email ${req.body.email} already exists` });
+                  if (res2.rows.length) { res.status(409).json({ success: false, message: `Email ${req.body.email} already exists` });
                   } else {
                       const hash = bcrypt.hashSync(req.body.password, 10);
-                      const query = { text: `insert into users ( name, email, password ) values ($1, $2, $3)returning id, name, email`,
-                          values: [ req.body.name, req.body.email, hash],
-                      };
+                      const query = { text: `insert into users ( name, email, password ) values ($1, $2, $3)returning id, name, email`, values: [ req.body.name, req.body.email, hash],};
                       return client.query(query, (error3, res3) => {
-                          if (error3) {
-                               res.status(400).json({ success: false, message: 'Something went wrong with the process, Please try later' });
+                          if (error3) { res.status(400).json({ success: false, message: 'Something went wrong with the process, Please try later' });
                           } else {
                               const createdUser = res3.rows[0];
                               const userToken = auth.authenticate(createdUser);
