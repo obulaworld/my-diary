6ed7e57efb923cf91a2b241765a7eb459e9c52df
obulaworld <obulaworldofexcellence@gmail.com>
2018-11-17 20:48:22 +0100

    chore(eslint) Setup ESLINT This chore sets up eslint closes 162031605

--- UI-REACT/src/components/NotFound.jsx
+++ UI-REACT/src/components/NotFound.jsx
@@ -1,15 +1,15 @@
- // react libraries
-import React, { Component } from 'react';
+// react libraries
+import React, { Component } from "react";
 /**
-* @desc renders home page
-*/
-class NotFound extends Component{
- render() {
-   return (
-     <div>
-       <h1>404..The page was not found</h1>
-     </div>
-   )
- }
+ * @desc renders home page
+ */
+class NotFound extends Component {
+  render() {
+    return (
+      <div>
+        <h1>404..The page was not found</h1>
+      </div>
+    );
+  }
 }
 export default NotFound;
--- UI-REACT/src/components/Router.jsx
+++ UI-REACT/src/components/Router.jsx
@@ -1,29 +1,29 @@
 // react library
-import React from 'react';
+import React from "react";
 
- // third party library
-import { Switch, Route, BrowserRouter  } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+// third party library
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { createBrowserHistory } from "history";
 
- // components
-import AuthenticatedRoute from './AuthRoute';
-import Home from './Home';
-import Dashboard from './Dashboard';
-import NotFound from './Dashboard';
-import Login from '../containers/Login';
+// components
+import AuthenticatedRoute from "./AuthRoute";
+import Home from "./Home";
+import Dashboard from "./Dashboard";
+import NotFound from "./Dashboard";
+import Login from "../containers/Login";
 
- const history = createBrowserHistory();
+const history = createBrowserHistory();
 /**
  * desc handles routing
  */
 const Router = () => (
-  <BrowserRouter history={ history }>
+  <BrowserRouter history={history}>
     <Switch>
-      <Route exact path='/' component={Home} />
-      <Route exact path='/login' component={Login} />
-      <AuthenticatedRoute exact path='/dashboard' component={Dashboard} />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <AuthenticatedRoute exact path="/dashboard" component={Dashboard} />
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
 );
- export default Router;
+export default Router;
