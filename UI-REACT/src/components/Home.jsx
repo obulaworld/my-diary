94ebf7650e9479cc021e3a8019df12edb561b7d7
obulaworld <obulaworldofexcellence@gmail.com>
2018-11-17 20:02:14 +0100

    chore(react-router) Setup React Router This chore sets up react router closes 162031875

---
+++ UI-REACT/src/components/Home.jsx
@@ -0,0 +1,15 @@
+ // react libraries
+ import React, { Component } from 'react';
+ /**
+ * @desc renders home page
+ */
+ class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is home</h1>
+      </div>
+    )
+  }
+ }
+ export default Home;
---
+++ UI-REACT/src/containers/Login.jsx
@@ -0,0 +1,15 @@
+ // react libraries
+ import React, { Component } from 'react';
+ /**
+ * @desc renders home page
+ */
+ class Login extends Component{
+  render() {
+    return (
+      <div>
+        <h1>Welcome to login</h1>
+      </div>
+    )
+  }
+ }
+ export default Login;
